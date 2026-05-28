ac11d53b379e8e02c89cdf99c8081cd6e85c721a
Safa <safaacikgoz43@gmail.com>
2026-05-28 04:10:56 +0300

    Update SimulationManager.java
    SimulationManagerde service providation methodlarina basladim ama gec oldugu icin yarina birakiyorum

--- src/main/java/com/objectville/engine/SimulationManager.java
+++ src/main/java/com/objectville/engine/SimulationManager.java
@@ -22,16 +22,16 @@ public class SimulationManager {
         Queue<Cell> queue = new LinkedList<>();
         Set<Cell> visited = new HashSet<>(); // sonsuz donguye girilmemesi icin hashset
 
-        queue.add(providerCell);
-        visited.add(providerCell);
+        queue.add(providerCell); // siraya soktuk
+        visited.add(providerCell); // hashsete soktuk
 
         int remainingAmount = amount;
 
         while (!queue.isEmpty() && remainingAmount > 0) {
-            Cell currentCell = queue.poll();
+            Cell currentCell = queue.poll(); // siradan aliyoruz
 
             if (currentCell instanceof Zone) {
-                Zone zone = (Zone) currentCell;
+                Zone zone = (Zone) currentCell; // consumeUtility kullanmak icin atadik
                 remainingAmount = zone.consumeUtility(utilityType, remainingAmount);
             }
             if (remainingAmount <= 0) {
@@ -39,12 +39,28 @@ public class SimulationManager {
             }
             List<Cell> neighbors = map.getNeighbors(currentCell.getX(), currentCell.getY());
             for (Cell neighbor : neighbors) {
-                if (!visited.contains(neighbor) && neighbor.isConnectable()) {
+                if (!visited.contains(neighbor) && neighbor.isConnectable()) { // eger hala kaynak varsa yoldan devam ediyoruz.
                     queue.add(neighbor);
                     visited.add(neighbor);
                 }
             }
         }
+    }
+
+    // servicelari paylastirmak icin gereken method
+    public void provideServices() {
+        Cell[][] grid = map.getCellGrid();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] instanceof Service) {
+                    Service service = (Service) grid[i][j];
+                    setRadius(service, grid); // teker teker service'lari arayip radiuslari ciziyoruz
+                }
+            }
+        }
+    }
+    // yaricaplari kullanarak alanlari olusturma methodu. daha kolay oldugu icin euclid kullanmaya karar verdim.
+    private void setRadius(Service service, Cell[][] grid) {
 
     }
 }
