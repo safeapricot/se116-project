765b1c9d0b9b44a14ba9784f3603718a667e13fc
ayberker <Ayberk.er@std.izmirekonomi.edu.tr>
2026-06-01 23:43:37 +0300

    Implemented total utility demand calculation and utility absorb.

--- src/main/java/com/objectville/engine/CityMap.java
+++ src/main/java/com/objectville/engine/CityMap.java
@@ -68,39 +68,39 @@ public class CityMap {
 
                 switch (letter) {
                     case 'E':
-                        cellGrid[i][j] = new Empty(i,j);
+                        cellGrid[i][j] = new Empty(i, j);
                         break;
                     case 'R':
-                        cellGrid[i][j] = new Road(i,j);
+                        cellGrid[i][j] = new Road(i, j);
                         break;
                     case 'T':
-                        cellGrid[i][j] = new InternetHub(i,j);
+                        cellGrid[i][j] = new InternetHub(i, j);
                         break;
                     case 'W':
-                        cellGrid[i][j] = new WaterPumpingStation(i,j);
+                        cellGrid[i][j] = new WaterPumpingStation(i, j);
                         break;
                     case 'I':
-                        cellGrid[i][j] = new Industrial(i,j);
+                        cellGrid[i][j] = new Industrial(i, j);
                         break;
                     case 'S':
-                        cellGrid[i][j] = new School(i,j);
+                        cellGrid[i][j] = new School(i, j);
                         break;
                     case 'D':
-                        cellGrid[i][j] = new Hospital(i,j);
+                        cellGrid[i][j] = new Hospital(i, j);
                         break;
                     case 'F':
-                        cellGrid[i][j] = new PoliceStation(i,j);
+                        cellGrid[i][j] = new PoliceStation(i, j);
                         break;
                     case 'H':
-                        cellGrid[i][j] = new Housing(i,j);
+                        cellGrid[i][j] = new Housing(i, j);
                         break;
                     case 'C':
-                        cellGrid[i][j] = new Commercial(i,j);
+                        cellGrid[i][j] = new Commercial(i, j);
                         break;
                     case 'P':
-                        cellGrid[i][j] = new PowerPlant(i,j);
+                        cellGrid[i][j] = new PowerPlant(i, j);
                         break;
-                        //sanırım bu kadar
+                    //sanırım bu kadar
                     default:
                         System.out.println("Error: Unknown letter!");
                         break;
@@ -110,7 +110,9 @@ public class CityMap {
         System.out.println("Cells successfully loaded!");
     }
 
-    public char[][] getGrid() {return charGrid; }
+    public char[][] getGrid() {
+        return charGrid;
+    }
 
     public Cell[][] getCellGrid() {
         return cellGrid;
@@ -118,7 +120,7 @@ public class CityMap {
 
     // alttaki method utilityprovider'in BFS algoritması için gerekli (komsularda isConnectable var mi diye bakacagiz)
     public List<Cell> getNeighbors(int x, int y) {
-        List <Cell> neighbors = new ArrayList<>();
+        List<Cell> neighbors = new ArrayList<>();
         int rowCount = charGrid.length;
         int colCount = charGrid[0].length;
 
@@ -135,4 +137,76 @@ public class CityMap {
 
 
     }
+
+    //TOPLAM TALEP
+    public int getTotalUtilityDemand() {
+        int totalDemand = 0;
+
+        for (int i = 0; i < cellGrid.length; i++) {
+            for (int j = 0; j < cellGrid[0].length; j++) {
+
+                char letter = charGrid[i][j];
+
+                if (letter == 'H' || letter == 'I' || letter == 'C') {
+
+                    totalDemand += cellGrid[i][j].getDemand();
+                    //Cell dosyasının içine bir getDemand methodu ekledim
+                }
+            }
+        }
+        return totalDemand;
+    }
+
+    public void moveUtility() {
+        for (int i = 0; i < cellGrid.length; i++) {
+            for (int j = 0; j < cellGrid[0].length; j++) {
+                char letter = charGrid[i][j];
+                if (letter == 'W' || letter == 'P' || letter == 'I') {
+
+                    runBFS(cellGrid[i][j]);
+                }
+            }
+        }
+        System.out.println("Distribute is completed");
+    }
+
+    //Burayı kesin KONTROL edin
+
+    public void runBFS(Cell providerCell){
+        ArrayList<Cell> queue = new ArrayList<>();
+        ArrayList<Cell> visited = new ArrayList<>();
+
+        queue.add(providerCell);
+        visited.add(providerCell);
+
+        String utilityType =providerCell.getUtilityType();
+        int availableUtility = providerCell.getOutput();
+
+        while(queue.size()>0 && availableUtility > 0){
+            Cell nextCell = queue.remove(0);
+            List<Cell> neighbors = getNeighbors(nextCell.getX(), nextCell.getY());
+
+            for (Cell neighbor : neighbors) {
+                if (visited.contains(neighbor)) {
+                }
+                else {
+                    visited.add(neighbor);
+                }
+
+                char neighborSymbol = charGrid[neighbor.getY()][neighbor.getX()];
+                if (neighborSymbol == 'R' ){
+                    queue.add(neighbor);
+                }
+                else if (neighborSymbol == 'H' || neighborSymbol == 'I' || neighborSymbol == 'C') {
+                    availableUtility = neighbor.consumeUtility(utilityType, availableUtility);
+                }
+            }
+        }
+    }
+
 }
+
+
+
+
+
--- src/main/java/com/objectville/model/Cell.java
+++ src/main/java/com/objectville/model/Cell.java
@@ -18,5 +18,26 @@ public abstract class Cell {
 
     @Override
     public String toString() { return String.valueOf(symbol); }
+
+    public int getDemand(){
+        return 0;
+    }
+
+    //Eğer cell, santral değilse boş döner
+    public String getUtilityType() {
+        return " ";
+    }
+
+    //Eğer cell, santral değilse üretim kapasitesi 0
+    public int getOutput() {
+        return 0;
+    }
+
+    //Eğer cell, bina değilse gelen elektriği/suyu harcamadan devam eder
+
+    public int consumeUtility(String utilityType, int incomingAmount) {
+        return incomingAmount;
+    }
+
 }
 
