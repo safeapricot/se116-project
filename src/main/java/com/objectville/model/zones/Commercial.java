1eb04483306e70248a63c60c6987663b22bc8559
ardatemel0 <arda.birinci@std.izmirekonomi.edu.tr>
2026-06-04 01:52:09 +0300

    fixed expection crush and implemented Commercial truely
    fixed expection crush and implemented Commercial truely

--- src/main/java/com/objectville/model/zones/Commercial.java
+++ src/main/java/com/objectville/model/zones/Commercial.java
@@ -2,13 +2,15 @@ package com.objectville.model.zones;
 
 public class Commercial extends Zone {
     public Commercial(int x, int y) { super(x, y, 'C'); }
-
     @Override
-    public void updateLevelAndOutput() {
+    public void updateLevelAndOutput(){
+
+
         int m = getMinUtility();
-       boolean level1Requirement = (m>0) && (receivedPopulation > 0) && (receivedGoods > 0);
-       boolean level2Requirement = level1Requirement && currentServices.get("Security");
-       boolean level3Requirement = level2Requirement && (receivedPopulation > 1);
+        boolean level1Requirement = (m > 0) && (receivedPopulation > 0) && (receivedGoods > 0);
+        boolean hasSecurity = currentServices.containsKey("Security") && currentServices.get("Security");
+        boolean level2Requirement = level1Requirement && hasSecurity;
+        boolean level3Requirement = level2Requirement && (receivedPopulation > 1);
 
         if (!level1Requirement) {
             level = 0;
@@ -19,9 +21,9 @@ public class Commercial extends Zone {
         } else if (level2Requirement) {
             if (level < 2) {
                 level++;
-            } else if (level > 2) {
-                level--;
-            }
+        } else if (level > 2) {
+            level--;
+        }
         } else if (level1Requirement) {
             if (level == 0) {
                 level++;
@@ -29,10 +31,12 @@ public class Commercial extends Zone {
                 level--;
             }
         }
+        if(level==0) output= 0;
+        else if(level==1) output =m;
+        else if(level==2) output= m * 2;
+        else output= m * 2 + Math.min( receivedPopulation, receivedGoods);
 
-    if (level == 0) output = 0;
-        else if (level == 1) output = m;
-        else if (level == 2) output = 2 * m;
-        else output = 2 * m + Math.min(receivedPopulation, receivedGoods);
+        }
     }
-}
+
+
